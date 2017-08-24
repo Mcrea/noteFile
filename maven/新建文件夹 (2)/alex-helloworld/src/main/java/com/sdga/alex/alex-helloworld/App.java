e1f7d0c4ed373d8f4a6ba6da98803d53333e0d95
Mcrea <704023655@qq.com>
2017-08-24 17:39:07 +0800

    first

---
+++ maven/新建文件夹 (2)/alex-helloworld/src/main/java/com/sdga/alex/alex-helloworld/App.java
@@ -0,0 +1,13 @@
+package com.sdga.alex.alex-helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
